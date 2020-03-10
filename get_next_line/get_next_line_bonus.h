dd485122fa394944bee35fecb4bb5877c81f6165
nadarm <ccc7497@gmail.com>
2020-03-11 02:54:48 +0900

    gnl bonus

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jko <jko@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/01 23:45:02 by jko               #+#    #+#             */
+/*   Updated: 2020/03/11 02:08:36 by jko              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 4096
+#endif
+
+
+int	get_next_line(int fd, char **line);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(char *str, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+#endif
---
+++ get_next_line/get_next_line_utils_bonus.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jko <jko@student.42seoul.kr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/07 14:56:40 by jko               #+#    #+#             */
+/*   Updated: 2020/03/10 20:56:51 by jko              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_strchr(char *str, int c)
+{
+	size_t i;
+	unsigned char	c2;
+
+	if (str == 0)
+		return (0);
+	c2 = (unsigned char)c;
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == c2)
+			return (str + i);
+		i++;
+	}
+	return (0);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	src_len;
+	size_t	i;
+	
+	src_len = ft_strlen(src);
+	if (dst == 0 || dstsize == 0)
+		return (src_len);
+	i = 0;
+	while (i < src_len && i + 1 < dstsize)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = 0;
+	return (src_len);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t len;
+
+	if (s == 0)
+		return (0);
+	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
